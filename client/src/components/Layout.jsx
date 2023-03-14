38a7ecdc5da9ca94ac339bd29c12a6edcc20d1f0
Synonymous64 <prajwalurkude007@gmail.com>
2023-03-14 20:43:04 +0530

    userslice redux created

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { sideBarMenu } from '../data/data';
 import '../styles/LayoutStyles.css';
+import { useSelector } from 'react-redux';
 
 const Layout = ({ children }) => {
+    const location = useLocation();
+    const { user } = useSelector(state => state.user);
     return (
         <div className='main'>
             <div className="layout">
@@ -14,19 +17,25 @@ const Layout = ({ children }) => {
                     </div>
                     <div className="menu">
                         {sideBarMenu.map(menu => {
+                            const isActive = location.pathname === menu.path;
                             return (
-                                <>
-                                    <div className="menu-item" key={menu.key}>
-                                        <i className={menu.icon}></i>
-                                        <Link to={menu.path}>{menu.name}</Link>
-                                    </div>
-                                </>
+
+                                <div className={`menu-item ${isActive && 'active'}`} key={menu.key}>
+                                    <i className={menu.icon}></i>
+                                    <Link to={menu.path}>{menu.name}</Link>
+                                </div>
+
                             )
                         })}
                     </div>
                 </div>
                 <div className="content">
-                    <div className="header">Header</div>
+                    <div className="header">
+                        <div className="header-content">
+                            <i class="fa-solid fa-bell"></i>
+                            <Link to="/profile">{user?.name}</Link>
+                        </div>
+                    </div>
                     <div className="body">{children}</div>
                 </div>
             </div>
